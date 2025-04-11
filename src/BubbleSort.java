fab390e69c76c0cae7c416e1a83371895ee5805f
Yogaraj <yogarajjuju22@gmail.com>
2025-04-11 18:57:41 +0530

    Tensa Zangetsu

---
+++ src/BubbleSort.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class BubbleSort {
+    public static void main(String[] args) {
+        int [] arr={2,5,3,1,4};
+        bubble(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void bubble(int[] arr){
+        boolean swapped;
+        for (int i = 0; i < arr.length; i++) {
+            swapped=false;
+            for (int j =1; j < arr.length-i; j++) {
+                if(arr[j]<arr[j-1]){
+                    int temp = arr[j];
+                    arr[j]=arr[j-1];
+                    arr[j-1]=temp;
+                    swapped =true;
+
+                }
+
+            }
+            if(!swapped){
+                break; //
+            }
+
+
+        }
+    }
+}
