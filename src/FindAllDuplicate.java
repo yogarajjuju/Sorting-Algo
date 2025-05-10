c6b65b0e92f875211b0e5edaa21811fe080f6ad2
Yogaraj <yogarajjuju22@gmail.com>
2025-05-10 18:17:55 +0530

    bankai

---
+++ src/FindAllDuplicate.java
@@ -0,0 +1,27 @@
+class FindAllDuplicate {
+    public List<Integer> findDuplicates(int[] nums) {
+         int i = 0;
+        while (i < nums.length) {
+            int correct = nums[i] - 1;
+            if (nums[i] != nums[correct]) {
+                swap(nums, i, correct);
+            } else {
+                i++;
+            }
+        }
+
+        List<Integer> ans = new ArrayList<>();
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != j + 1) {
+                ans.add(nums[j]);
+            }
+        }
+        return ans;
+    }
+
+    void swap(int[] nums, int first, int second) {
+        int temp = nums[first];
+        nums[first] = nums[second];
+        nums[second] = temp;
+    }
+}
