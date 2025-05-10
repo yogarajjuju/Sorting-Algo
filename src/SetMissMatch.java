8bb404aa88022801a316419b4a3a97eebf352c65
Yogaraj <yogarajjuju22@gmail.com>
2025-05-10 18:37:24 +0530

    bankai

---
+++ src/SetMissMatch.java
@@ -0,0 +1,25 @@
+class SetMissMatch {
+    public int[] findErrorNums(int[] nums) {
+        int i =0 ;
+        while(i<nums.length){
+            int crt = nums[i]-1;
+            if(nums[i]!= nums[crt]){
+                swap(nums, i , crt);
+            }
+            else{
+                i++;
+            }
+        }
+        for(int j = 0; j<nums.length;j++){
+            if(nums[j]!=j+1){
+                return new  int [] {nums[j], j+1};
+            }
+        }
+        return new int  [] {-1,-1};
+    }
+    void swap(int nums[] , int first ,int second){
+        int temp = nums[first];
+        nums[first]= nums[second];
+        nums[second]= temp;
+    }
+}
